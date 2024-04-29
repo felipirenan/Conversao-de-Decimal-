454b562ce85bc1413a6936f6829d21111459450a
felipirenan <felipirenan0@gmail.com>
2024-04-29 13:26:59 -0300

    Ajustado o menu, para que nao encerre o programa sem o usuario selecionar e mais alguns pequenos ajuste de formatacao de texto

--- ConversorDeDecimal.py
+++ ConversorDeDecimal.py
@@ -1,12 +1,12 @@
 curso = "Analise e Desenvolvimento de sistema "
 componentes_do_grupo = ["Renan Saran", "Lucas Givando", "Rodrigo"]
 disciplinas_envolvidas = ["Programação de Computadores", "Organização e Arquitetura de computadores"]
-versao_do_aplicativo = "1.2"
+versao_do_aplicativo = "1.3"
 
 
 
 def exibir_menu():
-    
+    print("\nMenu")
     print("1. Converter decimal")
     print("2. Curso")
     print("3. Componentes do Grupo")
@@ -48,40 +48,44 @@ def decimal_para_octal(decimal):
     
 
 def main():
-    exibir_menu()
 
-    opcao = int(input("\nEscolha uma opção: "))
-    
+    opcao = 0    
 
-    
-    if opcao == 1:
-        converterPara = int(input(" \n1. Binario \n2. Hexadecimal \n3. OctaDecimal \nConverter para: "))
-        decimal = int(input("\nDigite o decimal a ser convertido: "))
-        if converterPara == 1:
-            print("O decimal", decimal, "equivale a:", decimal_para_binario(decimal), "em Binario")
-        elif converterPara == 2:
-            print("O decimal", decimal, "equivale a:", decimal_para_hexadecimal(decimal), "em Hexadecimal")
-        elif converterPara == 3:
-            print("O decimal", decimal, "equivale a:", decimal_para_octal(decimal), "em OctaDecimal")
-            
-    elif opcao == 2:
-        print(curso)
+    while opcao != 6:
+        exibir_menu()
+        opcao = int(input("\nEscolha uma opção: "))
+        if opcao == 1:
+
+                converterPara = int(input(" \n1. Binario \n2. Hexadecimal \n3. OctaDecimal \nConverter para: "))
+                decimal = int(input("\nDigite o decimal a ser convertido: "))
+
+                if converterPara == 1:
+                    print("O decimal", decimal, "equivale a:", decimal_para_binario(decimal), "em Binario")
+                elif converterPara == 2:
+                    print("O decimal", decimal, "equivale a:", decimal_para_hexadecimal(decimal), "em Hexadecimal")
+                elif converterPara == 3:
+                    print("O decimal", decimal, "equivale a:", decimal_para_octal(decimal), "em OctaDecimal")
+
+        elif opcao == 2:
+            print(curso)
+
+        elif opcao == 3:
+            for componentes in componentes_do_grupo:
+                    print("- " + componentes)
+
+        elif opcao == 4:
+            for disciplina in disciplinas_envolvidas:
+                    print("- " + disciplina)
+
+        elif opcao == 5:
+            print(versao_do_aplicativo)
         
-    elif opcao == 3:
-        for componentes in componentes_do_grupo:
-                print("- " + componentes)
-    
-    elif opcao == 4:
-        for disciplina in disciplinas_envolvidas:
-                print("- " + disciplina)
-    
-    elif opcao == 5:
-        print(versao_do_aplicativo)
+        elif opcao == 6: 
+            print("Encerrando a aplicação...")
         
-   
+        else: 
+             print("Opção inválida. Por favor, escolha novamente.")
         
-
-
 main()
         
 
