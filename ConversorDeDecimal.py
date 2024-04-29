ab72bcc3a724bd0c0ef25152af78119851567fad
felipirenan <felipirenan0@gmail.com>
2024-04-29 12:27:40 -0300

    inicado o projeto e criado o menu de inicializacao

---
+++ ConversorDeDecimal.py
@@ -0,0 +1,23 @@
+curso = "Analise e Desenvolvimento de sistema "
+componentes_do_grupo = ["Renan Saran", "Lucas Givando", "Rodrigo"]
+disciplinas_envolvidas = ["Programação de Computadores", "Organização e Arquitetura de computadores"]
+versao_do_aplicativo = "1.0"
+
+
+
+def exibir_menu():
+    
+    print("1. Converter decimal")
+    print("2. Curso")
+    print("3. Componentes do Grupo")
+    print("4. Diciplinas")
+    print("5. Versão")
+    
+
+exibir_menu()
+
+
+    
+    
+    
+    
