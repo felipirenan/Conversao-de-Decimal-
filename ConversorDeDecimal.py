d7c4042c953424e1f6170bcde0bc6e8842af45a0
felipirenan <felipirenan0@gmail.com>
2024-04-29 13:14:58 -0300

    adcionada as funcoes para conversao de decimal

--- ConversorDeDecimal.py
+++ ConversorDeDecimal.py
@@ -1,7 +1,7 @@
 curso = "Analise e Desenvolvimento de sistema "
 componentes_do_grupo = ["Renan Saran", "Lucas Givando", "Rodrigo"]
 disciplinas_envolvidas = ["Programação de Computadores", "Organização e Arquitetura de computadores"]
-versao_do_aplicativo = "1.1"
+versao_do_aplicativo = "1.2"
 
 
 
@@ -16,16 +16,35 @@ def exibir_menu():
     
     
 def decimal_para_binario(decimal):
-  
-    return 
+    if decimal == 0:
+        return '0'
+    binario = ''
+    while decimal > 0:
+        binario = str(decimal % 2) + binario
+        decimal = decimal // 2
+    return binario
 
 def decimal_para_hexadecimal(decimal):
- 
-    return 
+    if decimal == 0:
+        return '0'
+    hexadecimal = ''
+    while decimal > 0:
+        resto = decimal % 16
+        if resto < 10:
+            hexadecimal = str(resto) + hexadecimal
+        else:
+            hexadecimal = chr(ord('A') + resto - 10) + hexadecimal
+        decimal = decimal // 16
+    return hexadecimal
 
 def decimal_para_octal(decimal):
-   
-    return 
+    if decimal == 0:
+        return '0'
+    octal = ''
+    while decimal > 0:
+        octal = str(decimal % 8) + octal
+        decimal = decimal // 8
+    return octal
     
 
 def main():
@@ -36,14 +55,14 @@ def main():
 
     
     if opcao == 1:
-        converterPara = int(input(" \n1. Binario \n2. Hexadecimal \n3. OctaDecimal \nConverter para:"))
+        converterPara = int(input(" \n1. Binario \n2. Hexadecimal \n3. OctaDecimal \nConverter para: "))
         decimal = int(input("\nDigite o decimal a ser convertido: "))
         if converterPara == 1:
-            resultado = decimal_para_binario(decimal)
+            print("O decimal", decimal, "equivale a:", decimal_para_binario(decimal), "em Binario")
         elif converterPara == 2:
-            resultado == decimal_para_hexadecimal(decimal)
+            print("O decimal", decimal, "equivale a:", decimal_para_hexadecimal(decimal), "em Hexadecimal")
         elif converterPara == 3:
-            resultado == decimal_para_octal(decimal)
+            print("O decimal", decimal, "equivale a:", decimal_para_octal(decimal), "em OctaDecimal")
             
     elif opcao == 2:
         print(curso)
